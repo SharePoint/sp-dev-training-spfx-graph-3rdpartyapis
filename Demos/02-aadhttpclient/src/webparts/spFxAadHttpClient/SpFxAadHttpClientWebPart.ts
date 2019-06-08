cbc264b98ee9f4fb52b98bfdb26c851ab34a7c82
Rob Windsor <rob.windsor@gmail.com>
2019-06-08 14:58:08 -0400

    Update project files for Ex 2

--- Demos/02-aadhttpclient/src/webparts/spFxAadHttpClient/SpFxAadHttpClientWebPart.ts
+++ Demos/02-aadhttpclient/src/webparts/spFxAadHttpClient/SpFxAadHttpClientWebPart.ts
@@ -4,11 +4,11 @@
 import * as React from 'react';
 import * as ReactDom from 'react-dom';
 import { Version } from '@microsoft/sp-core-library';
+import { BaseClientSideWebPart } from '@microsoft/sp-webpart-base';
 import {
-  BaseClientSideWebPart,
   IPropertyPaneConfiguration,
   PropertyPaneTextField
-} from '@microsoft/sp-webpart-base';
+} from '@microsoft/sp-property-pane';
 
 import * as strings from 'SpFxAadHttpClientWebPartStrings';
 import SpFxAadHttpClient from './components/SpFxAadHttpClient';
@@ -33,7 +33,7 @@ export default class SpFxAadHttpClientWebPart extends BaseClientSideWebPart<ISpF
               userItems: results
             }
         );
-
+  
         ReactDom.render(element, this.domElement);
       });
     }
