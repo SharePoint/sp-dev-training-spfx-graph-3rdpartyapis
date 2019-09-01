b384b29dd32d4e11a25d638f30764d15b0acd433
Rob Windsor <rob.windsor@gmail.com>
2019-08-31 22:20:07 -0400

    Update Ex 2 project files

--- Demos/02-aadhttpclient/src/webparts/spFxAadHttpClient/components/SpFxAadHttpClient.tsx
+++ Demos/02-aadhttpclient/src/webparts/spFxAadHttpClient/components/SpFxAadHttpClient.tsx
@@ -1,3 +1,6 @@
+// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT license.
+
 import * as React from 'react';
 import styles from './SpFxAadHttpClient.module.scss';
 import { ISpFxAadHttpClientProps } from './ISpFxAadHttpClientProps';
@@ -6,23 +9,23 @@ import { escape } from '@microsoft/sp-lodash-subset';
 export default class SpFxAadHttpClient extends React.Component<ISpFxAadHttpClientProps, {}> {
   public render(): React.ReactElement<ISpFxAadHttpClientProps> {
     return (
-      <div className={ styles.spFxAadHttpClient }>
-        <div className={ styles.container }>
-          <div className={ styles.row }>
-            <div className={ styles.column }>
-              <span className={ styles.title }>AadHttpClient Demo</span>
+      <div className={styles.spFxAadHttpClient}>
+        <div className={styles.container}>
+          <div className={styles.row}>
+            <div className={styles.column}>
+              <span className={styles.title}>AadHttpClient Demo</span>
             </div>
           </div>
 
-          <div className={ styles.row }>
+          <div className={styles.row}>
             <div><strong>Mail:</strong></div>
-            <ul className={ styles.list }>
-              { this.props.userItems &&
+            <ul className={styles.list}>
+              {this.props.userItems &&
                 this.props.userItems.map((user) =>
-                  <li key={ user.id } className={ styles.item }>
-                    <strong>ID:</strong> { user.id }<br />
-                    <strong>Email:</strong> { user.mail }<br />
-                    <strong>DisplayName:</strong> { user.displayName }
+                  <li key={user.id} className={styles.item}>
+                    <strong>ID:</strong> {user.id}<br />
+                    <strong>Email:</strong> {user.mail}<br />
+                    <strong>DisplayName:</strong> {user.displayName}
                   </li>
                 )
               }
